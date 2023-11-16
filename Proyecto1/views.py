367514c08dcebc0c6cb62cf3d4efa045127094fb
Agustin <correaagustin498@gmail.com>
2023-11-16 20:43:22 -0300

    Guardar archivo de django

---
+++ Proyecto1/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse  # se importa django
+from django.shortcuts import render
+
+def saludo(response):
+    return HttpResponse("Hola django")
+
+def saludo_html(response):
+    return  HttpResponse("<b>Hola django</b>")
+
+def saludo_nombre(response, nombre):
+    return HttpResponse(f"<h1>{nombre}<h1><br>Hola django")
+
+def saludo_plantilla(request):
+    contexto = {
+        "nombre" : "Agustin",
+        "edad": 22,
+        "hijos": [
+            {
+                "nombre": "Sol",
+                "edad": 15,
+                },
+            {
+                "nombre": "Celi",
+                "edad": 19,
+                }
+            ]
+        }
+    return render(request, template_name= "index.html", context= contexto)
